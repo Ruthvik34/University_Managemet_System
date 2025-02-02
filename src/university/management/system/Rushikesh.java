51bdbf05327c6f662047be1ee9773e08e8e7dc59
Ruthvik34 <81094389+Ruthvik34@users.noreply.github.com>
2025-02-02 18:25:23 +0530

    Update Rushikesh.java
    appa

--- src/university/management/system/Rushikesh.java
+++ src/university/management/system/Rushikesh.java
@@ -5,6 +5,9 @@ public class Rushikesh {
     public static void main(String[] args) {
         
         System.out.println("Hello Rushikesh");
+
+        System.out.println("Hello Rushikesh abscdef");
+        
     }
     
 }
